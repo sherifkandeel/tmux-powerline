67f02a36c930003161a66a367d6a86030df89b39
Sherif Mohamed <sherif.mohamed@zalando.de>
2018-10-20 00:24:11 +0200

    Added some changes for default theme

--- segments/lenny.sh
+++ segments/lenny.sh
@@ -1,5 +1,8 @@
+source "${TMUX_POWERLINE_DIR_LIB}/text_roll.sh"
+
 run_segment() {
 	# echo "( ͡° ͜ʖ ͡°)"
-	echo "¯\_(ツ)_/¯"
+	# echo "¯\_(ツ)_/¯"
+    # echo $(roll_text ${12} ${12} "(•_•)       ( •_•)>⌐■-■ (⌐■_■)      ( •_•)>⌐■-■ ")
 	return 0
 }
